32865619fb1014f6b474a93c1bdc59669a5d1a9e
moha100h <mmlu.tel@gmail.com>
2026-05-12 11:29:10 +0330

    feat: Widget receiver

---
+++ app/src/main/java/ir/moha/persiantodo/widget/TodoWidgetReceiver.kt
@@ -0,0 +1,8 @@
+package ir.moha.persiantodo.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class TodoWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TodoWidget()
+}
