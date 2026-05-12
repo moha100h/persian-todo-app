d6f2ffc69cb19a5607b0337f473ccfa26cc7728a
moha100h <mmlu.tel@gmail.com>
2026-05-12 11:14:53 +0330

    feat: AppDatabase Room

---
+++ app/src/main/java/ir/moha/persiantodo/data/local/AppDatabase.kt
@@ -0,0 +1,20 @@
+package ir.moha.persiantodo.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ir.moha.persiantodo.data.local.dao.CategoryDao
+import ir.moha.persiantodo.data.local.dao.TaskDao
+import ir.moha.persiantodo.data.local.entity.CategoryEntity
+import ir.moha.persiantodo.data.local.entity.TaskEntity
+
+@Database(
+    entities = [TaskEntity::class, CategoryEntity::class],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+    abstract fun categoryDao(): CategoryDao
+}
